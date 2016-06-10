4da1cbf5961cdf41ec0c8db7693a9deb92dda195
AL45 <allon.tran@rxpservices.com.au>
2016-06-10 14:30:39 +1000

    Add header validation test

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -4,9 +4,17 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 import org.mule.tck.junit4.FunctionalTestCase;
+import org.mule.api.MuleEvent;
+
 
 public class HelloMavenTest extends FunctionalTestCase {
-
+    @Test
+    public void retrieveFlightsAddsAppropriateHeader() throws Exception {
+    MuleEvent event = runFlow("retrieveFlights");
+    String contentType = event.getMessage().getOutboundProperty("Content-Type");
+    assertEquals("application/json", contentType);
+    }
+	
     @Test
     public void mavenFlowReturnsHelloMaven() throws Exception {
         runFlowAndExpect("mavenFlow", "Hello Maven");
@@ -16,5 +24,7 @@ public class HelloMavenTest extends FunctionalTestCase {
     protected String getConfigFile() {
         return "maven-project.xml";
     }
+
+
 
 }
